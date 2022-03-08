3487317384699e66378f16786494da2d30ae35fb
astronauthung <92112359+astronauthung@users.noreply.github.com>
2022-03-08 21:52:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const CONFIG = {
     titleWeb: "Mùng 8 tháng 3 em ra thăm zườn =))",
     introTitle: 'Wank Nki',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    introDesc: `I love myself... but I like me better when I'm with you`,
+    btnIntro: 'Nhấn zào đây ikk',
     title: 'Mong mình sẽ gặp nhau vào một ngày xớm nhấttt',
     desc: 'Pạn có iu mình hoq zạa ',
     btnYes: 'Cók',
@@ -13,7 +10,7 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà. Yêu bạn nhiềuuuuu 😘',
     messDesc: 'Mụt ngày đẹp trời mình sẽ đón pạn ik chơiii',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/nguyenhung409/' //link mess của các bạn. VD: https://m.me/nam.nodemy
