03601a20ddcdbf0665f67affe4873418a78bfc81
astronauthung <92112359+astronauthung@users.noreply.github.com>
2022-03-08 21:49:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    title: 'Mong mình sẽ gặp nhau vào một ngày xớm nhấttt',
+    desc: 'Pạn có iu mình hoq zạa ',
+    btnYes: 'Cók',
+    btnNo: 'Hong pé ui',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Mụt ngày đẹp trời mình sẽ đón pạn ik chơiii',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/nguyenhung409/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
